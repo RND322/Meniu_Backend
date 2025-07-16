2d53d63dee65b7c163d1b9f57a76bcc777b9aa56
RND322 <rubenrosales183@gmail.com>
2025-07-15 23:29:25 -0600

    Actualizacion

--- src/controllers/personas.controller.ts
+++ src/controllers/personas.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiBody, ApiResponse } from '@nestjs/swagger';
 import { PersonasService } from 'src/services/personas.service';
 import { CreatePersonaDto } from 'src/services/dto/create-persona.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { Roles } from 'src/auth/roles.decorator';
 
 @ApiTags('personas')
 @Controller('personas')
@@ -9,24 +12,31 @@ export class PersonasController {
   constructor(private readonly personasService: PersonasService) { }
 
   // Endpoint POST: Registrar a las personas
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('Gerente', 'Administrador')
   @Post('registrar')
   @HttpCode(HttpStatus.CREATED)
   @ApiOperation({
-    summary: 'Registrar una persona con usuario, rol y email',
-    description: "Crea un usuario con nombre de usuario, contraseña y lo enlaza con una persona y email."
+    summary: 'Registrar una persona con usuario, rol y email (Gestion) - Uso para Gerente, Administrador',
+    description:
+      "Crea un usuario con nombre de usuario, contraseña y lo enlaza con una persona y email. Sólo Gerente/Admin.",
   })
   @ApiBody({ type: CreatePersonaDto })
   @ApiResponse({
-    status: 201,
+    status: HttpStatus.CREATED,
     schema: {
       example: {
         nombre: 'Juan',
         apellidos: 'Pérez',
         email: 'juan@mail.com',
         nombre_usuario: 'JuanPérez489',
-        password: 'Juan!',
+        password: 'Juan!1234',
       },
     },
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Token inválido o sin permisos',
   })
   async registrar(@Body() dto: CreatePersonaDto) {
     return this.personasService.crearPersonaConUsuario(dto);
